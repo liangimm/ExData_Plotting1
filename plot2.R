1d7f85157b44e1cd720949588750ead4f2dffbec
immkoid@gmail.com <immkoid@gmail.com>
2015-08-07 10:04:51 +0800

    Assignment

---
+++ plot2.R
@@ -0,0 +1,18 @@
+setwd('C:\\Imm Document\\Coursea\\Exploratory Data Analysis\\Assignment01')
+
+# make sure the plots folder exists
+if (!file.exists('plots')) {
+  dir.create('plots')
+}
+
+# load data
+source('scripts/get_and_clean_data.R')
+
+# open device
+png(filename='plots/plot2.png',width=480,height=480,units='px')
+
+# plot data
+plot(power.consumption$DateTime,power.consumption$GlobalActivePower,ylab='Global Active Power (kilowatts)', xlab='', type='l')
+
+# close device
+x<-dev.off()
